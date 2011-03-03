61cf6aca5aed9e1358784982db448f66ac8a35db
Jonathan Hartley <tartley@tartley.com>
2011-03-03 14:43:03 +0000

    documented Event

--- gloopy/util/event.py
+++ gloopy/util/event.py
@@ -1,6 +1,25 @@
 
 class Event(object):
+    '''
+    Used when many subscribers would to recieve a function call from a single
+    sender.
 
+    .. function:: __init__()
+
+    Subscribe by in-place-add (+= or __iadd__) a callable of the right
+    signature. e.g::
+
+        def handler(this, that):
+            pass
+
+        event = Event()
+        event += handler
+
+        event.fire(1, 2)
+
+    The .fire call will invoke `handler`, passing 1 and 2, and will also
+    invoke all other subscribers to this event.
+    '''
     def __init__(self):
         self.listeners = []
 
@@ -9,6 +28,9 @@ class Event(object):
         return self
 
     def fire(self, *args, **kwargs):
+        '''
+        call all subscribers to the event
+        '''
         for listener in self.listeners:
             listener(*args, **kwargs)
 
