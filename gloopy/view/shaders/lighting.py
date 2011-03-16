322d8fd6142603abdf2041204657783004d2563a
Jonathan Hartley <tartley@tartley.com>
2011-03-16 09:26:38 -0400

    fixed lighting shader bug: directional lighting is now fixed w.r.t. world co-ords, which is correct, rather than eyespace co-ords. I had forgotten to multiply vertex normal by the normalmatrix

--- gloopy/view/shaders/lighting.py
+++ gloopy/view/shaders/lighting.py
@@ -9,23 +9,23 @@ varying vec4 baseColor;
 
 void main()
 {
-    vec4 ambientColor = vec4(0.325, 0.150, 0.007, 1.0);
-    vec3 lightDir = vec3(0.267, 0.267 * 3.0, 0.267 * 2.0);
-    vec4 lightColor = vec4(1.0, 1.0, 1.0, 1.0);
+    vec4 ambientLightColor = vec4(0.325, 0.150, 0.007, 1.0);
+    vec3 dirLightDir = vec3(0.267, 0.267 * 3.0, 0.267 * 2.0);
+    vec4 dirLightColor = vec4(1.0, 1.0, 1.0, 1.0);
 
     gl_Position = gl_ModelViewProjectionMatrix * vec4(position, 1.0);
-
-    float nDotL = dot(normal, lightDir);
+    
+    float nDotL = dot(gl_NormalMatrix * normal, dirLightDir);
  
     // fake lighting of backfaces too
     // perhaps remove this when we have more than one lightsource
     nDotL = 0.5 + nDotL * 0.5;
 
     nDotL = max(nDotL, 0.0);
-    vec4 diffuse_weight = color * lightColor;
+    vec4 diffuse_weight = color * dirLightColor;
 
     baseColor = (
-        ambientColor * color +
+        ambientLightColor * color +
         nDotL * diffuse_weight
     );
 }
