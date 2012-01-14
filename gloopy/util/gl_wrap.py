417f022aae68464743a345db502b8be732757973
Jonathan Hartley <tartley@tartley.com>
2012-01-14 12:35:00 +0000

    glGenVertexArrays apparently no longer needs a wrapper function - either because I'm on Windows today, or because PyOpenGL has been improved

--- gloopy/util/gl_wrap.py
+++ gloopy/util/gl_wrap.py
@@ -1,15 +1,11 @@
 '''
 Wrap any OpenGL functions which are fiddly to access via the raw bindings.
 '''
-
-from OpenGL import GL
 from OpenGL.GL.ARB import vertex_array_object
 
 def glGenVertexArray():
     '''
     Return the integer ID of the created vertex object array
     '''
-    vao_id = GL.GLuint(0)
-    vertex_array_object.glGenVertexArrays(1, vao_id)
-    return vao_id.value
+    return vertex_array_object.glGenVertexArrays(1)
 
