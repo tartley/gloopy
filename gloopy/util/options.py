ea813330a59d10c458993fbab63b72ee565f0abb
Jonathan Hartley <tartley@tartley.com>
2012-06-23 12:23:45 +0100

    make vsync default, change option to 'nosync'

--- gloopy/util/options.py
+++ gloopy/util/options.py
@@ -9,20 +9,19 @@ class Options(object):
     args. One day they might also take config files into account, to allow
     persistance of option changes by the user.
 
-    `vsync`: Boolean, default to True. synchronise screen refreshes to monitor.
-    Setting this to True will limit framerate to your monitor refresh rate
+    `nosync`: Boolean, Don't synchronise screen refreshes to monitor.
+    By default, framerate is limited to your monitor refresh rate
     (e.g. 60fps), which is likely what you want unless you are performance
-    testing. Setting it to False will increase framerate, but not visibly
+    testing. Specifying this flag will increase framerate, but not visibly
     (since your monitor cannot display the extra frames) and will introduce
-    'tearing'. Note that some video drivers have settings which override this
-    value.
+    'tearing'. Some video drivers have settings which override this value.
 
     `fullscreen`: boolean, default to True. If false, display in a window.
 
     `fps`: Display frames per second in lower-left of screen.
     '''
     def __init__(self, argv):
-        self.vsync = '--vsync' in argv
+        self.vsync = '--nosync' not in argv
         self.fullscreen = '--window' not in argv and '-w' not in argv
         self.fps = '--fps' in argv
         log.info(self)
