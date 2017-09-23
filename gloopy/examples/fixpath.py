ccc3ff08813d7bfdec60425b56fb363f8f9f4fde
Jonathan Hartley <tartley@tartley.com>
2017-09-22 23:43:31 -0500

    Promote examples/browser.py to top level run.py.
    Delete the other examples. They were not maintained and didn't run any more.

--- gloopy/examples/fixpath.py
+++
@@ -1,7 +0,0 @@
-
-# make sure we can find gloopy in '..', so that we can run from within
-# 'examples' dir, even if Gloopy isn't installed.
-import sys
-from os.path import abspath, dirname, join
-sys.path.insert(0, abspath(join(dirname(__file__), '..', '..')))
-
